4ac9524369c8918204fc66c427b02add63b87b24
hyiromori <hyiromori@gmail.com>
2020-03-21 20:25:45 +0900

    Split file.

---
+++ src/app.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react'
+
+export const App: React.FC = () => {
+  return (<h1>Markdown Editor</h1>)
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import { render } from 'react-dom'
+import { App } from './app'
 import './index.css'
 
-const Main = (<h1>Markdown Editor</h1>)
+const Main = (<App />)
 
 render(Main, document.getElementById('app'))
