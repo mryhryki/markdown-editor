3c9d35f219b32f372e98356d0fcb6da2d59d6b95
hyiromori <hyiromori@gmail.com>
2020-03-22 06:50:07 +0900

    Use `useState`.

--- src/app.tsx
+++ src/app.tsx
@@ -1,14 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const App: React.FC = () => {
+  const [text, setText] = useState<string>('')
+
   return (
     <div id="wrapper">
       <header>
         Markdown Editor
       </header>
       <div id="editor">
-        <textarea id="markdown-text">Text Area</textarea>
-        <div id="markdown-preview">Preview Area</div>
+        <textarea
+          id="markdown-text"
+          onChange={(event) => setText(event.target.value)}
+          value={text}
+        />
+        <div id="markdown-preview">{text}</div>
       </div>
     </div>
   )
