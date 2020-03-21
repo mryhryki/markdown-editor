ecf64705231d8b5073584b11cb7918131c57065d
hyiromori <hyiromori@gmail.com>
2020-03-21 18:13:37 +0900

    Add typescript config in webpack.config.js.

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,18 @@ const path = require('path')
 
 module.exports = {
   entry: './src/index.js',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
