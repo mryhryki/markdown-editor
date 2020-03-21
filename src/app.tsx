5273a97d731d448caa506344080491a43488502d
hyiromori <hyiromori@gmail.com>
2020-03-21 20:28:00 +0900

    Add content.

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,15 @@
 import * as React from 'react'
 
 export const App: React.FC = () => {
-  return (<h1>Markdown Editor</h1>)
+  return (
+    <div id="wrapper">
+      <header>
+        <h1>Markdown Editor</h1>
+      </header>
+      <div id="editor">
+        <textarea id="markdown-text">Text Area</textarea>
+        <div id="markdown-preview">Preview Area</div>
+      </div>
+    </div>
+  )
 }
