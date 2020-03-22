e6765bf2450c9fd15131827187a897fe6208956e
hyiromori <hyiromori@gmail.com>
2020-03-23 06:11:50 +0900

    Confirm create database by dexie.

--- src/app.tsx
+++ src/app.tsx
@@ -4,11 +4,13 @@ import React, {
 } from 'react'
 import { getQueryParams } from './util/query_params'
 import { Preview } from './component/preview'
+import { getHistory } from './util/history'
 
 export const App: React.FC = () => {
   const [text, setText] = useState<string>('')
 
   useEffect(() => {
+    getHistory('test').then(console.log)
     const queryParams = getQueryParams(window.location.search)
     if (queryParams.text != null && queryParams.text.trim() !== '') {
       setText(queryParams.text)
