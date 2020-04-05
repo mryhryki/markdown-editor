527eac1551a1926f6d1a0cf19b0d9ffbeb1f6d4e
hyiromori <hyiromori@gmail.com>
2020-04-05 17:42:33 +0900

    Open cache.

--- sw.js
+++ sw.js
@@ -1,7 +1,16 @@
+const CACHE_NAME = 'Cache:1.0.0'
+
 self.addEventListener('install', (event) => {
   console.log('ServiceWorker install:', event)
+  event.waitUntil(
+    caches
+      .open(CACHE_NAME)
+      .then(() => console.log('Opened cache:', CACHE_NAME)),
+  )
 })
 
 self.addEventListener('activate', (event) => {
   console.log('ServiceWorker activate:', event)
 })
+
+
