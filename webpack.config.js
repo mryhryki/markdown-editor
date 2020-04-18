5ebc2de67c270c3df835c10c7378091d434c0dcd
hyiromori <hyiromori@gmail.com>
2020-04-18 16:16:11 +0900

    Webpack の設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,10 @@
 const path = require('path')
-const { GenerateSW } = require('workbox-webpack-plugin')
 
 module.exports = {
-  entry: './src/index.tsx',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader'],
-      },
-    ],
-  },
-  plugins: [
-    new GenerateSW({
-      swDest: path.resolve(__dirname, 'sw.js'),
-      runtimeCaching: [
-        {
-          handler: 'NetworkFirst',
-          urlPattern: /.*/,
-        },
-      ],
-    }),
-  ],
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
+  entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     publicPath: 'dist/',
-  },
-  devServer: {
-    hot: true,
-    open: true,
-  },
+  }
 }
