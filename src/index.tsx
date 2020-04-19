26179cea9115f928f88b9f878146c6ff063f1c68
hyiromori <hyiromori@gmail.com>
2020-04-19 09:06:11 +0900

    Add webpack-dev-server.

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 
-const Main = (<h1>Markdown Editor</h1>)
+const Main = (<h1>Markdown Editor + Dev Server</h1>)
 
 render(Main, document.getElementById('app'))
--- webpack.config.js
+++ webpack.config.js
@@ -19,4 +19,8 @@ module.exports = {
     filename: 'index.js',
     publicPath: 'dist/',
   },
+  devServer: {
+    hot: true,
+    open: true,
+  }
 }
