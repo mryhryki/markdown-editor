d32e5c355df8ee8a704b85b2df791416c5423ca1
hyiromori <hyiromori@gmail.com>
2020-06-06 23:01:48 +0900

    Add put function.

--- src/indexeddb/memos.ts
+++ src/indexeddb/memos.ts
@@ -9,3 +9,8 @@ export interface MemoRecord {
 const database = new Dexie('markdown-editor')
 database.version(1).stores({ memos: '&datetime' })
 const memos: Dexie.Table<MemoRecord, string> = database.table('memos')
+
+export const putMemo = async (title: string, text: string): Promise<void> => {
+  const datetime = new Date().toISOString()
+  await memos.put({ datetime, title, text })
+}
