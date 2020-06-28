bf6b05d291dc1395107e98ab9ba78d4d168a08fc
hyiromori <hyiromori@gmail.com>
2020-06-28 11:34:10 +0900

    4-3.2

--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -2,6 +2,13 @@ import * as React from 'react'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 import { Header } from '../components/header'
+import { useState } from 'react'
+import {
+  getMemos,
+  MemoRecord,
+} from '../indexeddb/memos'
+
+const { useEffect } = React
 
 const HeaderArea = styled.div`
   position: fixed;
@@ -19,7 +26,34 @@ const Wrapper = styled.div`
   padding: 0 1rem;
 `
 
+const Memo = styled.button`
+  display: block;
+  background-color: white;
+  border: 1px solid gray;
+  width: 100%;
+  padding: 1rem;
+  margin: 1rem 0;
+  text-align: left;
+`
+
+const MemoTitle = styled.div`
+  font-size: 1rem;
+  margin-bottom: 0.5rem;
+`
+
+const MemoText = styled.div`
+  font-size: 0.85rem;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
 export const History: React.FC = () => {
+  const [memos, setMemos] = useState<MemoRecord[]>([])
+
+  useEffect(() => {
+    getMemos().then(setMemos)
+  }, [])
 
   return (
     <>
@@ -31,7 +65,14 @@ export const History: React.FC = () => {
         </Header>
       </HeaderArea>
       <Wrapper>
-        TODO: 履歴表示
+        {memos.map((memo) => {
+          return (
+            <Memo key={memo.datetime}>
+              <MemoTitle>{memo.title}</MemoTitle>
+              <MemoText>{memo.text}</MemoText>
+            </Memo>
+          )
+        })}
       </Wrapper>
     </>
   )
