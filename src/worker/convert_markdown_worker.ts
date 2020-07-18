57883368849ac06460f21424c7f9cf6e9b02fc40
hyiromori <hyiromori@gmail.com>
2020-07-18 23:18:30 +0900

    5-3-4.

--- src/worker/convert_markdown_worker.ts
+++ src/worker/convert_markdown_worker.ts
@@ -1,12 +1,9 @@
+import * as marked from 'marked'
+
 const worker: Worker = self as any
 
 worker.addEventListener('message', (event) => {
-  console.log('Worker Received:', event.data)
-
-  let count: number = 1
-  while (count < 1_000_000_000) { // 先程設定した値に合わせてください
-    count++
-  }
-
-  worker.postMessage({ result: event.data })
+  const text = event.data
+  const html = marked(text)
+  worker.postMessage({ html })
 })
