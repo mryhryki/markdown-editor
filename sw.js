af6ac100ac4d2eaca8adfea20887f923f0389c16
hyiromori <hyiromori@gmail.com>
2020-08-05 09:22:18 +0900

    6-2-3.

--- sw.js
+++ sw.js
@@ -5,3 +5,8 @@ self.addEventListener('install', (event) => {
 self.addEventListener('activate', (event) => {
   console.log('ServiceWorker activate:', event)
 })
+
+self.addEventListener('fetch', (event) => {
+  console.log('Fetch to:', event.request.url)
+  event.respondWith(fetch(event.request));
+});
