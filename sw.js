56f48af9971c59ee17818403776ce01166b78108
hyiromori <hyiromori@gmail.com>
2020-08-06 17:49:56 +0900

    6-2-4.

--- sw.js
+++ sw.js
@@ -1,3 +1,5 @@
+const CacheName = 'Cache:v1'
+
 self.addEventListener('install', (event) => {
   console.log('ServiceWorker install:', event)
 })
@@ -6,7 +8,18 @@ self.addEventListener('activate', (event) => {
   console.log('ServiceWorker activate:', event)
 })
 
+const networkFallingBackToCache = async (request) => {
+  const cache = await caches.open(CacheName)
+  try {
+    const response = await fetch(request)
+    await cache.put(request, response.clone())
+    return response
+  } catch (err) {
+    console.error(err)
+    return cache.match(request)
+  }
+}
+
 self.addEventListener('fetch', (event) => {
-  console.log('Fetch to:', event.request.url)
-  event.respondWith(fetch(event.request));
-});
+  event.respondWith(networkFallingBackToCache(event.request))
+})
